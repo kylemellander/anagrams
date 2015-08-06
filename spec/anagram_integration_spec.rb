4d5f8e528cec01e90509890a63930c0603194f4d
Kyle Mellander & Andrew Haupt <student@epicodus.com>
2015-08-06 15:09:35 -0700

    Create basic layout page and add form to index page and add test for one anagram

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -8,4 +8,11 @@ describe('/', {:type => :feature}) {
     visit("/")
     expect(page).to have_content("Anagrams!")
   }
+  it("returns anagrams on the results page when only one possibility given") {
+    visit("/")
+    fill_in("origin_word", :with => "Andrew")
+    fill_in("possible_anagrams", :with => "warden")
+    click_button('Go!')
+    expect(page).to have_content("warden")
+  }
 }
