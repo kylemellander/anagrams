88a6e425807d2bb837db0f7ab0baedc1c5dd3685
Kyle Mellander & Andrew Haupt <student@epicodus.com>
2015-08-06 16:43:13 -0700

    Formatting for blank inputs and directed anagrams page to index if going directly

--- app.rb
+++ app.rb
@@ -8,16 +8,20 @@ get ('/') {
 }
 
 get ('/anagrams') {
-  @original_word_error = 1 if params.fetch('origin_word') == ""
-  @possible_anagrams_error = 1 if params.fetch('possible_anagrams') == ""
-  @original_word = params.fetch('origin_word')
-  @original_anagrams = params.fetch('possible_anagrams')
-  @possible_anagrams = params.fetch('possible_anagrams').gsub(/\s+/, "").split(",")
-
-  if @original_word_error != 1 && @possible_anagrams_error != 1
-    @results = @original_word.anagram(@possible_anagrams)
-    erb(:results)
-  else
+  if params.empty?
     erb(:index)
+  else
+    @original_word_error = 1 if params.fetch('origin_word') == ""
+    @possible_anagrams_error = 1 if params.fetch('possible_anagrams') == ""
+    @original_word = params.fetch('origin_word')
+    @original_anagrams = params.fetch('possible_anagrams')
+    @possible_anagrams = params.fetch('possible_anagrams').gsub(/\s+/, "").split(",")
+
+    if @original_word_error != 1 && @possible_anagrams_error != 1
+      @results = @original_word.anagram(@possible_anagrams)
+      erb(:results)
+    else
+      erb(:index)
+    end
   end
 }
