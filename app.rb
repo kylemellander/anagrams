f3957ccfc09798b903d4c7670bd946bbd3ac1efa
Kyle Mellander & Andrew Haupt <student@epicodus.com>
2015-08-06 15:24:59 -0700

    Add launchy gem

--- app.rb
+++ app.rb
@@ -8,7 +8,17 @@ get ('/') {
 }
 
 get ('/anagrams') {
-  @original_word = params.fetch("origin_word")
-  @possible_anagrams = params.fetch("possible_anagrams").gsub(/\s+/, "").split(",")
-  @results = @original_word.anagram(@possible_anagrams)
+  if !params.fetch("origin_word")
+    @original_word_error = 1
+  elsif !params.fetch("possible_anagrams")
+    @possible_anagrams_error = 1
+  else
+    @original_word = params.fetch("origin_word")
+    @possible_anagrams = params.fetch("possible_anagrams").gsub(/\s+/, "").split(",")
+    @results = @original_word.anagram(@possible_anagrams)
+    erb(:results)
+  end
+  if @original_word_error = 1 || @possible_anagrams_error = 1
+    erb(:index)
+  end
 }
