0de0d84cbb7f3bb6755151dfa3e03d41d55b57ed
Kyle Mellander & Andrew Haupt <student@epicodus.com>
2015-08-06 14:22:49 -0700

    Pass test handling case of one argument that matches

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -9,4 +9,8 @@ describe('/') {
   it("returns message if no anagrams are found") {
     expect("fox".anagram(["duck", "hippo"])).to(eq("There are no anagrams."))
   }
+
+  it("returns the anagram if 1 argument is given and is an anagram") {
+    expect("fox".anagram(["xfo"])).to(eq(["xfo"]))
+  }
 }
